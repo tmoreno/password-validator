be1effffb3e53f2cc5e84eeb4baa951641965670
tmorenobernal <tmorenobernal@gmail.com>
2021-10-30 19:23:34 +0200

    Added test that check if the password length is less than 8 chars

--- java/src/test/java/com/tmoreno/kata/passwordvalidator/PasswordValidatorTest.java
+++ java/src/test/java/com/tmoreno/kata/passwordvalidator/PasswordValidatorTest.java
@@ -2,13 +2,16 @@ package com.tmoreno.kata.passwordvalidator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PasswordValidatorTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void should_throw_invalid_length_exception_when_password_length_is_less_than_8_chars() {
+		assertThrows(InvalidLengthException.class, () -> {
+			PasswordValidator passwordValidator = new PasswordValidator();
+			passwordValidator.validate("1234567");
+		});
 	}
 
 }
