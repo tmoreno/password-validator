9d39cc9e23c5c2a9f9529139d85626324f666798
tmorenobernal <tmorenobernal@gmail.com>
2021-10-30 20:12:21 +0200

    Refactor. Extract methods

--- java/src/main/java/com/tmoreno/kata/passwordvalidator/PasswordValidator.java
+++ java/src/main/java/com/tmoreno/kata/passwordvalidator/PasswordValidator.java
@@ -3,22 +3,42 @@ package com.tmoreno.kata.passwordvalidator;
 public final class PasswordValidator {
 
     public void validate(String password) {
+        haveMoreThan8Chars(password);
+
+        containsACapitalLetter(password);
+
+        containsALowerLetter(password);
+
+        containsANumber(password);
+
+        containsAnUnderscore(password);
+    }
+
+    private void haveMoreThan8Chars(String password) {
         if (password.length() <= 8) {
             throw new InvalidLengthException();
         }
+    }
 
+    private void containsACapitalLetter(String password) {
         if (password.equals(password.toLowerCase())) {
             throw new NoCapitalLettersException();
         }
+    }
 
+    private void containsALowerLetter(String password) {
         if (password.equals(password.toUpperCase())) {
             throw new NoLowerLettersException();
         }
+    }
 
+    private void containsANumber(String password) {
         if (!password.matches(".*\\d.*")) {
             throw new NoNumbersException();
         }
+    }
 
+    private void containsAnUnderscore(String password) {
         if (!password.contains("_")) {
             throw new NoUnderscoreException();
         }
