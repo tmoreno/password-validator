10261cb501a0ee203c9024505089f36bade18bd6
tmorenobernal <tmorenobernal@gmail.com>
2021-10-30 19:46:53 +0200

    Move method

--- java/src/test/java/com/tmoreno/kata/passwordvalidator/PasswordValidatorTest.java
+++ java/src/test/java/com/tmoreno/kata/passwordvalidator/PasswordValidatorTest.java
@@ -7,6 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class PasswordValidatorTest {
 
 	@Test
+	public void should_valid_password_when_password_length_is_more_than_8_chars_and_contains_a_capital_letter() {
+		PasswordValidator passwordValidator = new PasswordValidator();
+		passwordValidator.validate("12345678A");
+	}
+
+	@Test
 	public void should_throw_invalid_length_exception_when_password_length_is_less_than_8_chars() {
 		assertThrows(InvalidLengthException.class, () -> {
 			PasswordValidator passwordValidator = new PasswordValidator();
@@ -20,12 +26,6 @@ public class PasswordValidatorTest {
 			PasswordValidator passwordValidator = new PasswordValidator();
 			passwordValidator.validate("12345678");
 		});
-	}
-
-	@Test
-	public void should_valid_password_when_password_length_is_more_than_8_chars_and_contains_a_capital_letter() {
-		PasswordValidator passwordValidator = new PasswordValidator();
-		passwordValidator.validate("12345678A");
 	}
 
 	@Test
