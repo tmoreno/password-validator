c6729c13c28b98f557ab57b9cd700f07fe180177
tmorenobernal <tmorenobernal@gmail.com>
2021-10-30 19:29:54 +0200

    Added test that check if the password length is 8 chars

--- java/src/test/java/com/tmoreno/kata/passwordvalidator/PasswordValidatorTest.java
+++ java/src/test/java/com/tmoreno/kata/passwordvalidator/PasswordValidatorTest.java
@@ -14,4 +14,12 @@ public class PasswordValidatorTest {
 		});
 	}
 
+	@Test
+	public void should_throw_invalid_length_exception_when_password_length_is_8_chars() {
+		assertThrows(InvalidLengthException.class, () -> {
+			PasswordValidator passwordValidator = new PasswordValidator();
+			passwordValidator.validate("12345678");
+		});
+	}
+
 }
